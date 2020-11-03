283da07cab13b46329505025d004c50790d6183e
sam <sam@freighttrust.com>
2020-11-03 04:08:33 -0800

    feat(gh): commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  singleQuote: false,
+  trailingComma: "es5",
+  proseWrap: "never",
+  endOfLine: "lf",
+};
